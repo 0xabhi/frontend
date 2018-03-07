4c97a710a9fcc2855c24c0b133cc561806b0cbf5
Raman Shalupau <raman.shalupau@gmail.com>
2018-03-07 12:40:03 +0800

    improved testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,8 +4,6 @@ import 'slick-carousel/slick/slick-theme.css'
 
 import React from 'react';
 import { Image, Icon, Rating } from 'semantic-ui-react'
-import Slider from 'react-slick'
-
 
 class Testimonials extends React.Component {
   constructor (props) {
@@ -22,21 +20,19 @@ class Testimonials extends React.Component {
   }
 
   render () {
-    return <Slider {...this.settings} className='testimonials'>
+    return <div className='testimonials'>
       <div>
         <Icon name='quote left' color='grey' size='large' />
         <p className='quote'>
-          Let me give you my feedback. I think it would be useful to you. Your site drives <b>more qualified leads than AngelList</b>.
-          <br/>
-          Thanks for the follow up! Next position I’ll repeat with you.
+          At first, I was hesitant. But then decided to <b>feature</b>… And <b>it worked</b>!<br/> The quality of the <b>candidates are by far more relevant</b> then on other boards. And, I mean, I don’t need to explain crypto to them!
         </p>
         <div className='author'>
-          <a href='https://twitter.com/kleros_io' target='_blank'>
-            <Image centered circular size='tiny' src='https://res.cloudinary.com/cryptojobslist/image/fetch/g_face,c_thumb,w_90,h_90,e_improve,q_auto,fl_lossy,f_auto/dpr_2.0/https://res.cloudinary.com/cryptojobslist/image/upload/v1515861481/r3agu0zrctzu5mnqrhvp.jpg' />
+          <a href='https://twitter.com/Davie_Michael' target='_blank'>
+            <Image centered circular size='tiny' src='https://res.cloudinary.com/cryptojobslist/image/fetch/g_face,c_thumb,w_90,h_90,e_improve,q_auto,fl_lossy,f_auto/dpr_2.0/https://res.cloudinary.com/cryptojobslist/image/upload/v1519053954/w23nhpncdqfcpaxda7il.jpg' />
           </a>
           <p>
-            <b><a href='https://twitter.com/kleros_io' target='_blank'>Maria T. Vidal</a></b><br/>
-            Communications Lead, <a href="https://cryptojobslist.com/blockchain-companies/kleros" target='_blank'>Kleros</a>
+            <b><a href='https://twitter.com/Davie_Michael' target='_blank'>Mike Davie</a></b><br/>
+            Founder & CEO, <a href="https://cryptojobslist.com/blockchain-companies/datastreamx" target='_blank'>DataStreamX</a>
             <br/>
             <Rating maxRating={5} defaultRating={5} icon='star' size='tiny' disabled/><br/>
           </p>
@@ -62,21 +58,23 @@ class Testimonials extends React.Component {
       <div>
         <Icon name='quote left' color='grey' size='large' />
         <p className='quote'>
-          At first, I was hesitant. But then decided to <b>feature</b>… And <b>it worked</b>!<br/> The quality of the <b>candidates are by far more relevant</b> then on other boards. And, I mean, I don’t need to explain crypto to them!
+          Let me give you my feedback. I think it would be useful to you. Your site drives <b>more qualified leads than AngelList</b>.
+          <br/>
+          Thanks for the follow up! Next position I’ll repeat with you.
         </p>
         <div className='author'>
-          <a href='https://twitter.com/Davie_Michael' target='_blank'>
-            <Image centered circular size='tiny' src='https://res.cloudinary.com/cryptojobslist/image/fetch/g_face,c_thumb,w_90,h_90,e_improve,q_auto,fl_lossy,f_auto/dpr_2.0/https://res.cloudinary.com/cryptojobslist/image/upload/v1519053954/w23nhpncdqfcpaxda7il.jpg' />
+          <a href='https://twitter.com/kleros_io' target='_blank'>
+            <Image centered circular size='tiny' src='https://res.cloudinary.com/cryptojobslist/image/fetch/g_face,c_thumb,w_90,h_90,e_improve,q_auto,fl_lossy,f_auto/dpr_2.0/https://res.cloudinary.com/cryptojobslist/image/upload/v1515861481/r3agu0zrctzu5mnqrhvp.jpg' />
           </a>
           <p>
-            <b><a href='https://twitter.com/Davie_Michael' target='_blank'>Mike Davie</a></b><br/>
-            Founder & CEO, <a href="https://cryptojobslist.com/blockchain-companies/datastreamx" target='_blank'>DataStreamX</a>
+            <b><a href='https://twitter.com/kleros_io' target='_blank'>Maria T. Vidal</a></b><br/>
+            Communications Lead, <a href="https://cryptojobslist.com/blockchain-companies/kleros" target='_blank'>Kleros</a>
             <br/>
             <Rating maxRating={5} defaultRating={5} icon='star' size='tiny' disabled/><br/>
           </p>
         </div>
       </div>
-    </Slider>
+    </div>
   }
 }
 
