bd91cd19e42e2fe392928067d26827bf019e060c
Raman Shalupau <raman.shalupau@gmail.com>
2018-05-03 10:25:31 +0800

    3k twitter followers

--- src/components/PostAJob.js
+++ src/components/PostAJob.js
@@ -231,7 +231,7 @@ class PostAJob extends React.Component {
               <h3>Verified <span className='ui text strikethrough'>$150</span> <span className='ui green text'>$99</span></h3>
               <ul>
                 <li>📩 Unlimited Applicants</li>
-                <li><i className="fab fa-twitter ui text blue"/> Reach 2.2k+ targeted <a href='https://twitter.com/cryptojobslist' target='_blank'><b>Twitter</b></a> followers </li>
+                <li><i className="fab fa-twitter ui text blue"/> Reach 3k+ targeted <a href='https://twitter.com/cryptojobslist' target='_blank'><b>Twitter</b></a> followers </li>
                 <li><i className="fab fa-reddit ui text orange"/> Get auto-published on 5 #crypto sub-<span className="ui text orange"><b>Reddits</b></span>  </li>
                 <li><i className="fab fa-telegram-plane ui text dark-blue"/> Notify our <a href='https://t.me/cryptojobslist' className='ui text dark-blue' target='_blank'><b>Telegram</b></a> channel followers</li>
                 <li>✅ Published within 12 hours</li>
