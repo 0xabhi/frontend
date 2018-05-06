42c9695b349a3a0b13b7e93c061ad342cd53f604
Raman Shalupau <raman.shalupau@gmail.com>
2018-05-06 09:23:57 +0800

    making footer lightweight

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
 import './Footer.styl';
 
 import React from 'react';
-import { Container, Grid, Segment } from 'semantic-ui-react'
+import { Container, Grid, Segment, Divider } from 'semantic-ui-react'
 
 class Footer extends React.Component {
   render() {
     return (
+      <Container className='Footer' text>
+        <p color="text grey">Crypto Jobs List &copy; {(new Date()).getFullYear()}</p>
+      </Container>
+    )
+    return (
       <Grid className='Footer' columns='equal' divided inverted padded>
         <Grid.Row color='black' textAlign='center'>
           <Grid.Column>
-            <Segment color='black' inverted >Crypto Jobs List &copy; {(new Date()).getFullYear()}</Segment>
           </Grid.Column>
           <Grid.Column>
             <Segment color='black' inverted></Segment>
